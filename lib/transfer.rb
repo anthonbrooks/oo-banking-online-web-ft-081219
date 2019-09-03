c0064c7f56c36022535eb32235c971027368edd2
Anthony Prescott-Brooks <anthonybrooks226@gmail.com>
2019-09-03 20:10:16 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -13,7 +13,7 @@ class Transfer
     sender.valid? && receiver.valid?
   end 
   
-  def execute_transaction
+  def execute_transaction(amount)
     receiver.balance += sender.amount if sender.balance >= @amount
   end 
   
